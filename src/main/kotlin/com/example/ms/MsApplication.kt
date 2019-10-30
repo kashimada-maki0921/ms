943e1296f696b029e4c1a0c9a83d2e2213d32835
鹿島田真生 <kashimada.maki@videomarket.co.jp>
2019-10-30 11:35:28 +0900

    雛形作成

---
+++ src/main/kotlin/com/example/ms/MsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsApplication>(*args)
+}
