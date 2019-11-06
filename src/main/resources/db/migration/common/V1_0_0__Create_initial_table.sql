5cf0676b6d8712cef336ae78419beaa0c7e8c1be
鹿島田真生 <kashimada.maki@videomarket.co.jp>
2019-11-06 20:05:17 +0900

    resolve #8 ユーザー身体管理テーブルとprotoを作成

--- src/main/resources/db/migration/common/V1_0_0__Create_initial_table.sql
+++ src/main/resources/db/migration/common/V1_0_0__Create_initial_table.sql
@@ -10,3 +10,16 @@ CREATE TABLE users
   DEFAULT CHARSET = utf8mb4 COMMENT ='ユーザーテーブル';
 
 SET FOREIGN_KEY_CHECKS = 1;
+
+CREATE TABLE users_body
+(
+  id         INT UNSIGNED,
+  user_id    INT UNSIGNED,
+  sex        ENUM('男', '女') COLLATE 'utf8mb4_bin'  NOT NULL COMMENT '性別',
+  height     INT,
+  weight     INT,
+  created_at DATETIME            DEFAULT CURRENT_TIMESTAMP                             NOT NULL COMMENT '登録日時',
+  updated_at DATETIME            DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL COMMENT '更新日時',
+  PRIMARY KEY (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4 COMMENT ='ユーザー身体管理テーブル';
